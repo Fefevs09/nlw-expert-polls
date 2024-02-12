ca369908d559c45d5473542e9636291414025d93
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-02-12 20:16:31 -0300

    feat:create websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,31 +1,28 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
-import { string, z } from 'zod'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { createPoll } from './routes/post-polls'
+import { getPoll } from './routes/get-polls'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie from "@fastify/cookie"
+import { pollResult } from './ws/poll-result'
+import  websocket from '@fastify/websocket'
 
 const app = fastify()
 const PORT = 3000
 
-const prisma = new PrismaClient()
-
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string()
-  })
-
-  const { title } = createPollBody.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  })
-
-  return reply.status(201).send({ pollId: poll.id })
+app.register(cookie, {
+    secret: "qualquercoisa",
+    hook: 'onRequest',
 })
 
-app.get('/', () => {
-  return 'Hello World'
-})
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
+app.register(pollResult)
 
 app.listen({ port: PORT }).then(() => {
   console.log(`HTTP server running in: http://localhost:${PORT}`);
